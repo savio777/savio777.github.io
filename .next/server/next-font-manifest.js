142c69ef34da803b0738c9f5d9350c62e0c9504e
Sávio <saviopf22@gmail.com>
2023-12-12 01:28:34 -0300

    publish

---
+++ .next/server/next-font-manifest.js
@@ -0,0 +1,1 @@
+self.__NEXT_FONT_MANIFEST='{"pages":{"/_app":["static/media/fda9a09c4f7f92aa-s.p.woff2","static/media/7777133e901cd5ed-s.p.woff2"],"/":["static/media/916d3686010a8de2-s.p.woff2"]},"app":{},"appUsingSizeAdjust":false,"pagesUsingSizeAdjust":true}';
